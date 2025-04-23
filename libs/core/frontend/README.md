72eae30b87185d9f9f9d109ce70c0e28ac2a23c6
Hoàng Phi <phi.hoang@herond.org>
2025-04-23 17:14:12 +0700

    feat: add libs

---
+++ libs/core/frontend/README.md
@@ -0,0 +1,7 @@
+# @herond-portal/frontend
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @herond-portal/frontend` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/core/frontend/src/lib/frontend.tsx
@@ -0,0 +1,15 @@
+
+import styles from './frontend.module.css';
+
+export function Frontend() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Frontend!</h1>
+    </div>
+  );
+}
+
+export default Frontend;
+
+
+
